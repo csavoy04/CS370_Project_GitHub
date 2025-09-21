8a8e6cc15f44b67b1b375113aac4b50b9a1ae490
Adel-Boodhan <boodhanap@gmail.com>
2025-09-21 17:49:57 -0400

    Main Menu Update
    Made the main menu turn off when pressing the play button.

--- CS370/Assets/UI/Main_Menu_Controller.cs
+++ CS370/Assets/UI/Main_Menu_Controller.cs
@@ -12,13 +12,20 @@ public class Main_Menu_Controller : MonoBehaviour
         root = uiDocument.rootVisualElement;
         
         var quitBtn = root.Q<VisualElement>("QuitBtn");
-        quitBtn.RegisterCallback<ClickEvent>(OnClickEvent);
+        quitBtn.RegisterCallback<ClickEvent>(QuitEvent);
 
+        var playBtn = root.Q<VisualElement>("PlayBtn");
+        playBtn.RegisterCallback<ClickEvent>(PlayEvent);
     }
 
     // Update is called once per frame
-    private void OnClickEvent(ClickEvent evt)
+    private void QuitEvent(ClickEvent evt)
     {
         Debug.Log("Quitting Game");
+    }
+
+    private void PlayEvent(ClickEvent evt)
+    {
+        gameObject.SetActive(false);
     }
 }
