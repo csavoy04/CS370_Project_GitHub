2f33a36cfb7c3fb1b821b73f44f798a18e8fa624
ColorfastTub26 <jadencheung1@gmail.com>
2025-09-30 15:29:34 -0400

    Updated Combat Handler Script
    Added Enemy Party System to the script, should be very similar/the same to the Player Party System

--- CS370/Assets/Scripts/combatHandler.cs
+++ CS370/Assets/Scripts/combatHandler.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 
 public enum BattleState { PlayerTurn, EnemyTurn, Start, Won, Lost, Neutral }
 
+<<<<<<< Updated upstream
 public class combatHandler : MonoBehaviour
 {
     /*
+=======
+public class combatHandler : MonoBehaviour {
+>>>>>>> Stashed changes
     public GameObject playerprefab;
     public GameObject enemyprefab;
     public BattleState State;
 
-    //Party List
+    //Player Party List
     public List<Character> PlayerParty;
+
+    //Enemy Party List
+    public List<Enemy> EnemyParty;
+
     void Start()
     {
         State = BattleState.Neutral;
@@ -19,7 +27,7 @@ public class combatHandler : MonoBehaviour
         State = BattleState.Start;
         Debug.Log("Combat State Start");
 
-        //Create Party (for testing)
+        //Create Player Party (for testing)
         PlayerParty = new List<Character>();
 
         PlayerParty.Add(new Character("Timmy", 999, 999, 999, 999));
@@ -27,38 +35,38 @@ public class combatHandler : MonoBehaviour
         PlayerParty.Add(new Character("Bob", 5, 5, 5, 5));
 
         Debug.Log("Party Member 2 Name: " + PlayerParty[1].Name + " || Party Member 2 Lvl: " + PlayerParty[1].Level);
+
+        //Create Enemy Party (for testing)
+        EnemyParty = new List<Enemy>();
+
+        EnemyParty.Add(new Enemy("Slimmy", 1, 20, 20, 5));
+        EnemyParty.Add(new Enemy("Slimey", 1, 20, 20, 5));
+        EnemyParty.Add(new Enemy("Slim", 1, 20, 20, 5));
     }
 
-    void Update()
-    {
-        if (State == BattleState.Start)
-        {
+    void Update(){
+        if (State == BattleState.Start){
             SpawnEnemy();
             SpawnPlayerTeam();
             PlayerTurn();
         }
     }
-    public void PlayerTurn()
-    {
+    public void PlayerTurn(){
         Input.GetKeyDown(KeyCode.V);
         Debug.Log("Pressed V");
         State = BattleState.EnemyTurn;
         Debug.Log("Combat State Enemy Turn");
     }
-    public void SpawnEnemy()
-    {
+    public void SpawnEnemy(){
         int NoOfEnemies = 3;
-        for (NoOfEnemies = 0; NoOfEnemies < 3; NoOfEnemies++)
-        {
+        for (NoOfEnemies = 0; NoOfEnemies < 3; NoOfEnemies++){
             Instantiate(enemyprefab, new UnityEngine.Vector3(3, NoOfEnemies + 1, NoOfEnemies * 2), UnityEngine.Quaternion.identity);
         }
 
     }
-    public void SpawnPlayerTeam()
-    {
+    public void SpawnPlayerTeam(){
         int NoOfAllies = 3;
-        for (NoOfAllies = 0; NoOfAllies < 3; NoOfAllies++)
-        {
+        for (NoOfAllies = 0; NoOfAllies < 3; NoOfAllies++){
             Instantiate(playerprefab, new UnityEngine.Vector3(-3, NoOfAllies, NoOfAllies * 2), UnityEngine.Quaternion.identity);
         }
     } 
@@ -66,8 +74,7 @@ public class combatHandler : MonoBehaviour
 }
 
 //Party System
-public struct Character
-{
+public struct Character {
     //Stats
     public string Name { get; set; }
     public int Level { get; set; }
@@ -76,8 +83,23 @@ public struct Character
     public int Attack { get; set; }
 
     //Contructor
-    public Character(string GivenName, int GivenLevel, int GivenHealth, int GivenMana, int GivenAttack)
-    {
+    public Character(string GivenName, int GivenLevel, int GivenHealth, int GivenMana, int GivenAttack){
+        Name = GivenName;
+        Health = GivenHealth;
+        Mana = GivenMana;
+        Attack = GivenAttack;
+    }
+}
+
+public struct Enemy {
+    //Stats
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public int Health { get; set; }
+    public int Mana { get; set; }
+    public int Attack { get; set; }
+
+    public Enemy(string GivenName, int GivenLevel, int GivenHealth, int GivenMana, int GivenAttack){
         Name = GivenName;
         Health = GivenHealth;
         Mana = GivenMana;
