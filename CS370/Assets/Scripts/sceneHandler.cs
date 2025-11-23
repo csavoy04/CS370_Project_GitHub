ce79ee2191cfd2388908712d375fee1bdfd08851
csavoy04 <csavoy04@gmail.com>
2025-11-22 20:01:15 -0500

    Add worlds 3 and 4, general bug fixes

--- CS370/Assets/Scripts/sceneHandler.cs
+++ CS370/Assets/Scripts/sceneHandler.cs
@@ -28,7 +28,12 @@ public class sceneHandler : MonoBehaviour
         {"TestAreaBlackSmith", new Vector3(-6.8f, 2f, 7f)},
         {"World2TestArea", new Vector3(-18f, 2f, 0f)},
         {"World2BlackSmith", new Vector3(-4.4f, 2f, 7.3f)},
-        {"TestAreaWorld2", new Vector3(100f, 2f, 0f)}
+        {"TestAreaWorld2", new Vector3(100f, 2f, 0f)},
+        {"World3World2", new Vector3(-20f, 2f, 0f)},
+        {"World2World3", new Vector3(158f, 2f, 0f)},
+        {"World3World4", new Vector3(104f, 2f, 0f)},
+        {"World4World3", new Vector3(-20f, 2f, 0f)},
+        {"World4BlackSmith", new Vector3(-6.8f, 2f, 7f)}
     };
 
     public void Start()
