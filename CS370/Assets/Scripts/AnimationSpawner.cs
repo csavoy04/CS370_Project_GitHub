50d2aac390ff0c9debfc5422baef5992c5afed93
ColorfastTub26 <jadencheung1@gmail.com>
2025-12-02 12:58:49 -0500

    Fixed Non-static Move Animation to depend on duration
    Non-static Move Animations like Fireball and Ice Bullet now go from point A to B within it's animation duration.

--- CS370/Assets/Scripts/AnimationSpawner.cs
+++ CS370/Assets/Scripts/AnimationSpawner.cs
@@ -1,27 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 public class AnimationSpawner : MonoBehaviour
 {
+
     public GameObject bitePrefab;
     public GameObject lightningPrefab;
     public GameObject FireBallPreFab;
-
-    public GameObject iceSpikePrefab;
-
+    public GameObject IceBulletPrefab;
     public GameObject tacklePrefab;
-
     public GameObject stompPrefab;
 
     public Transform spawnPoint;
 
-    public void SpawnAnimation(Unit Defender, string MoveName, float duration)
+    float DefenderX = 0;
+    float DefenderY = 0;
+    float DefenderZ = 0;
+
+    float AttackerX = 0;
+    float AttackerY = 0;
+    float AttackerZ = 0;
+
+    public Vector3 SpawnPos;
+    public Vector3 TargetPos;
+
+    public bool IsMovingAnimation = false;
+
+    public string CurrentMoveName;
+
+    public void SpawnAnimation(Unit Attacker, Unit Defender, string MoveName, float duration)
     {
-        float x = 0;
-        float z = 0;
+        CurrentMoveName = MoveName;
+        GameObject animation;
 
-        float offsety = 0;
-
-        if(Defender.GetPartyClass() == "Player")
+        if (Defender.GetPartyClass() == "Player")
         {
             int allyCount = PartySystem.Instance.PlayerParty != null ? PartySystem.Instance.PlayerParty.Count : 0;
             for (int NoOfAllies = 0; NoOfAllies < allyCount; NoOfAllies++)
@@ -30,26 +42,57 @@ public class AnimationSpawner : MonoBehaviour
                 {
                     if (Defender == PartySystem.Instance.PlayerParty[NoOfAllies])
                     {
-                        z = (NoOfAllies - 1) * 3 - 0.7f;
+                        DefenderZ = (NoOfAllies - 1) * 3 - 0.7f;
                     }
                 }
             }
 
-            x = -4.85f;
-        } 
+            DefenderX = -4.85f;
+        }
         else
         {
             int enemyCount = PartySystem.Instance.EnemyParty != null ? PartySystem.Instance.EnemyParty.Count : 0;
             for (int NoOfEnemies = 0; NoOfEnemies < enemyCount; NoOfEnemies++)
             {
-                if (PartySystem.Instance.EnemyParty[NoOfEnemies] == Defender) 
+                if (PartySystem.Instance.EnemyParty[NoOfEnemies] == Defender)
                 {
-                    z = (NoOfEnemies - 1) * 3 - 0.7f;
+                    DefenderZ = (NoOfEnemies - 1) * 3 - 0.7f;
                 }
-                
+
             }
 
-            x = 4.85f;
+            DefenderX = 4.85f;
+        }
+
+        if (Attacker.GetPartyClass() == "Player")
+        {
+            int allyCount = PartySystem.Instance.PlayerParty != null ? PartySystem.Instance.PlayerParty.Count : 0;
+            for (int NoOfAllies = 0; NoOfAllies < allyCount; NoOfAllies++)
+            {
+                if (PartySystem.Instance.PlayerParty[NoOfAllies].GetPartyClass() != "Empty" && PartySystem.Instance.PlayerParty[NoOfAllies].GetCurrentHealth() > 0)
+                {
+                    if (Attacker == PartySystem.Instance.PlayerParty[NoOfAllies])
+                    {
+                        AttackerZ = (NoOfAllies - 1) * 3 - 0.7f;
+                    }
+                }
+            }
+
+            AttackerX = -4.85f;
+        }
+        else
+        {
+            int enemyCount = PartySystem.Instance.EnemyParty != null ? PartySystem.Instance.EnemyParty.Count : 0;
+            for (int NoOfEnemies = 0; NoOfEnemies < enemyCount; NoOfEnemies++)
+            {
+                if (PartySystem.Instance.EnemyParty[NoOfEnemies] == Defender)
+                {
+                    AttackerZ = (NoOfEnemies - 1) * 3 - 0.7f;
+                }
+
+            }
+
+            AttackerX = 4.85f;
         }
 
         switch (Defender.GetUnitClass())
@@ -57,50 +100,81 @@ public class AnimationSpawner : MonoBehaviour
             case "Warrior":
             case "Mage":
             case "Rogue":
-                offsety = 1;
+                DefenderY = 1;
                 break;
             case "Slime":
-                offsety = 1;
+                DefenderY = 1;
                 break;
             default:
-                offsety = 0;
+                DefenderY = 0;
                 break;
         }
 
-        Vector3 spawnPos = new Vector3(x, offsety, z);
-        GameObject animation;
+        switch (Attacker.GetUnitClass())
+        {
+            case "Warrior":
+            case "Mage":
+            case "Rogue":
+                AttackerY = 1;
+                break;
+            case "Slime":
+                AttackerY = 1;
+                break;
+            default:
+                AttackerY = 0;
+                break;
+        }
+
+        switch (MoveName)
+        {
+            case "Fireball":
+            case "Ice Spike":
+                SpawnPos = new Vector3(AttackerX, AttackerY, AttackerZ);
+                TargetPos = new Vector3(DefenderX, DefenderY, DefenderZ);
+                IsMovingAnimation = true;
+                break;
+            default:
+                SpawnPos = new Vector3(DefenderX, DefenderY, DefenderZ);
+                break;
+        }
+
         switch (MoveName)
         {
             case "Bite":
-                animation = Instantiate(bitePrefab, spawnPos, Quaternion.identity);
-                Destroy(animation, duration);
+                animation = Instantiate(bitePrefab, SpawnPos, Quaternion.identity);
                 break;
             case "Lightning Bolt":
-                animation = Instantiate(lightningPrefab, spawnPos, Quaternion.identity);
-                Destroy(animation, duration);
+                animation = Instantiate(lightningPrefab, SpawnPos, Quaternion.identity);
                 break;
             case "Fireball":
-                animation = Instantiate(FireBallPreFab, spawnPos, Quaternion.identity);
-                Destroy(animation, duration);
+                animation = Instantiate(FireBallPreFab, SpawnPos, Quaternion.identity);
                 break;
             case "Ice Spike":
-                animation = Instantiate(iceSpikePrefab, spawnPos, Quaternion.identity);
-                Destroy(animation, duration);
+                animation = Instantiate(IceBulletPrefab, SpawnPos, Quaternion.identity);
                 break;
             case "Tackle":
-                animation = Instantiate(tacklePrefab, spawnPos, Quaternion.identity);
-                Destroy(animation, duration);
+                animation = Instantiate(tacklePrefab, SpawnPos, Quaternion.identity);
                 break;
             case "Stomp":
-                animation = Instantiate(stompPrefab, spawnPos, Quaternion.identity);
-                Destroy(animation, duration);
+                animation = Instantiate(stompPrefab, SpawnPos, Quaternion.identity);
                 break;
             default:
-                animation = Instantiate(bitePrefab, spawnPos, Quaternion.identity);
-                Destroy(animation, duration);
+                animation = Instantiate(bitePrefab, SpawnPos, Quaternion.identity);
                 break;
 
         }
+        StartCoroutine(TimerCoroutine(duration));
+        Destroy(animation, duration);
         animation.transform.forward = Camera.main.transform.forward;
+    }
+
+    IEnumerator TimerCoroutine(float Seconds)
+    {
+
+        //Start Timer
+        yield return new WaitForSeconds(Seconds);
+
+        IsMovingAnimation = false;
+
     }
 }
--- CS370/Assets/Scripts/BillboardSprite.cs
+++ CS370/Assets/Scripts/BillboardSprite.cs
@@ -2,9 +2,53 @@ using UnityEngine;
 
 public class BillboardSprite : MonoBehaviour
 {
+    public AnimationSpawner animationSpawner;
+
+    Vector3 CurrentPos;
+    Vector3 TargetDirection;
+
+    private float elapsed = 0f;
+    private float duration;
+
+    public AnimationClip animationClip;
+
+    void Start()
+    {
+        animationSpawner = Object.FindFirstObjectByType<AnimationSpawner>();
+
+        CurrentPos = animationSpawner.SpawnPos;
+        TargetDirection = (animationSpawner.TargetPos - CurrentPos).normalized;
+
+        // Get the animation clip duration
+        switch(animationSpawner.CurrentMoveName)
+        {
+            case "Fire Ball":
+                duration = animationSpawner.FireBallPreFab.GetComponent<Animator>().runtimeAnimatorController.animationClips[0].length;
+                break;
+            case "Ice Spike":
+                duration = animationSpawner.IceBulletPrefab.GetComponent<Animator>().runtimeAnimatorController.animationClips[0].length;
+                break;
+            default:
+                duration = 1f; // Default duration if move name is not recognized
+                break;
+        }
+    }
+
     void LateUpdate()
     {
         if (Camera.main)
             transform.forward = Camera.main.transform.forward;
+    }
+
+    private void Update()
+    {
+        if (animationSpawner.IsMovingAnimation)
+        {
+            elapsed += Time.deltaTime;
+
+            float t = Mathf.Clamp01(elapsed / duration);
+
+            transform.position = Vector3.Lerp(animationSpawner.SpawnPos, animationSpawner.TargetPos, t);
+        }
     }
 }
